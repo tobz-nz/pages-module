1141b6ab674977c990be463fcf7c9e2b04116b26
Toby Evans <tobz.nz@gmail.com>
2015-12-01 16:56:29 +1300

    fix de language file

--- resources/lang/de/permission.php
+++ resources/lang/de/permission.php
@@ -2,22 +2,28 @@
 
 return [
     'pages'  => [
-        'title'  => 'Seiten',
-        'read'   => 'Kann Seiten anschauen?',
-        'write'  => 'Kann Seiten erstellen oder ändern?',
-        'delete' => 'Kann Seiten löschen?'
+        'name'  => 'Seiten',
+        'option' => [
+            'read'   => 'Kann Seiten anschauen?',
+            'write'  => 'Kann Seiten erstellen oder ändern?',
+            'delete' => 'Kann Seiten löschen?'
+        ]
     ],
     'types'  => [
-        'title'  => 'Typen',
-        'read'   => 'Kann Typen anschauen?',
-        'write'  => 'Kann Typen erstellen oder ändern?',
-        'delete' => 'Kann Typen löschen?',
-        'fields' => 'Kann Felder von Typen anpassen?'
+        'name'  => 'Typen',
+        'option' => [
+            'read'   => 'Kann Typen anschauen?',
+            'write'  => 'Kann Typen erstellen oder ändern?',
+            'delete' => 'Kann Typen löschen?',
+            'fields' => 'Kann Felder von Typen anpassen?'
+        ]
     ],
     'fields' => [
-        'title'  => 'Felder',
-        'read'   => 'Kann Felder anschauen?',
-        'write'  => 'Kann Felder schreiben?',
-        'delete' => 'Kann Felder löschen?'
+        'name'  => 'Felder',
+        'option' => [
+            'read'   => 'Kann Felder anschauen?',
+            'write'  => 'Kann Felder schreiben?',
+            'delete' => 'Kann Felder löschen?'
+        ]
     ]
 ];
